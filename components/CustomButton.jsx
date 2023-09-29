f8049343e18fc915aaf80e21134a2d3ff0100771
Patrício Samuel <patricioanjosjf@gmail.com>
2023-09-29 19:32:49 -0300

    Criação de botão personalizado

---
+++ components/CustomButton.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function CustomButton({ title, containerStyles, handleClick }) {
+    return (
+        <button
+            disabled={false}
+            type="button"
+            className={`custom-btn ${containerStyles}`}
+            onClick={handleClick}
+        >
+            <span className={`flex-1`}>{title}</span>
+        </button>
+    )
+}
