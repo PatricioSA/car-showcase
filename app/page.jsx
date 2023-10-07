a520b582e7b69545820c274e87105c641a2333c7
Patrício Samuel <patricioanjosjf@gmail.com>
2023-10-07 15:49:34 -0300

    iniciando seção de catálogos

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,27 @@
-import { HeroSection } from '@/components'
+import { CustomFilter, HeroSection, SearchBar } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <HeroSection/>
+      <HeroSection />
+
+      <section className='mt-12 padding-x padding-y
+      max-width' id='discover'>
+        <div>
+          <h1>Catálogo de Carros</h1>
+          <p>Explore os carros que você gostaria de ter</p>
+        </div>
+
+        <div>
+          <SearchBar/>
+
+          <div>
+            <CustomFilter/>
+            <CustomFilter/>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
--- components/index.js
+++ components/index.js
@@ -2,10 +2,14 @@ import HeroSection from "./HeroSection";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
 export {
     HeroSection,
     CustomButton,
     Navbar,
     Footer,
+    SearchBar,
+    CustomFilter,
 }
